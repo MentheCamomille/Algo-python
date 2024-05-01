6b31b937f86962983e7abcac67c8dbc05a8710fb
Shayman <96345953+MentheCamomille@users.noreply.github.com>
2024-05-01 12:19:18 +0200

    Update merge_sort.py

--- merge_sort/merge_sort.py
+++ merge_sort/merge_sort.py
@@ -5,14 +5,10 @@ def merge_sort(arr, visualize=False):
     if len(arr) <= 1:
         return arr
 
-    # Divide the array into two halves.
     mid = len(arr) // 2
     left = merge_sort(arr[:mid], visualize)
-    # Recursively sort the left half
     right = merge_sort(arr[mid:], visualize)
-    # Recursively sort the right half
 
-    # Merge the two sorted halves.
     merged = []
 
     i = 0
@@ -26,9 +22,7 @@ def merge_sort(arr, visualize=False):
             j += 1
 
     merged.extend(left[i:])
-    # Append any remaining elements from the left half
     merged.extend(right[j:])
-    # Append any remaining elements from the right half
 
     if visualize:
         visualize_sorting(merged)
